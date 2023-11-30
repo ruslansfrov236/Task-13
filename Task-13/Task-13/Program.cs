8592fa7024bc1218aa59137b5293befe7c3261ba
Ruslan Seferov <91841957+ruslansfrov236@users.noreply.github.com>
2023-11-30 14:22:22 +0400

    Add files via upload

---
+++ Task-13/Task-13/Program.cs
@@ -0,0 +1,22 @@
+﻿
+        Thread th1 = new Thread(CreateContent);
+        Thread th2 = new Thread(ContinueKey);
+
+        th1.Start();
+        th1.Join();
+        th2.Start();
+    
+
+    static void ContinueKey()
+    {
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    static void CreateContent()
+    {
+       
+        string data = Console.ReadLine();
+        
+    }
+
